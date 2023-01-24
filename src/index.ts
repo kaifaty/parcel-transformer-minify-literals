861ec65c6a87f7d0de84d3d46ed28d828e8b4223
Kaifaty <andry.diego2011@gmail.com>
2023-01-24 23:42:37 +0500

    move to dependency

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import {minifyHTMLLiterals} from './minify'
-import {Transformer} from '@parcel/plugin'
+import { minifyHTMLLiterals } from "minify-literals";
+import { Transformer } from "@parcel/plugin";
 
 export default new Transformer({
-  async transform({asset}) {
-    const source = await asset.getCode()
-    const res = await minifyHTMLLiterals(source)
+  async transform({ asset }) {
+    const source = await asset.getCode();
+    const res = await minifyHTMLLiterals(source);
     if (res && res.code) {
-      asset.setCode(res.code)
+      asset.setCode(res.code);
     }
-    return [asset]
+    return [asset];
   },
-})
+});
